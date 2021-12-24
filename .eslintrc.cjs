6d8aa62f7152104cb083dba22d373526df53c74e
Michał Marchewczyk <dev@marchewczyk.eu>
2021-12-24 14:39:09 +0100

    Setup SvelteKit

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript'),
+		'svelte3/ignore-styles': () => true,
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	},
+	rules: {
+		indent: [
+			'warn',
+			'tab'
+		],
+		quotes: [
+			'warn',
+			'single'
+		],
+		semi: [
+			'warn',
+			'always'
+		],
+		'@typescript-eslint/no-explicit-any': 2,
+		'no-console': 1,
+	}
+};
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Docker Control Panel</title>
+</svelte:head>
+
+<slot></slot>
+
+<style lang="scss">
+  :global {
+	@import '../styles/_Fonts.scss';
+	@import '../styles/_Typography.scss';
+  }
+</style>
