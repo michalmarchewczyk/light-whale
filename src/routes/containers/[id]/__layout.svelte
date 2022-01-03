e15ac59d8ebcef6198ccdc8d30ef67dab70455cb
Michał Marchewczyk <dev@marchewczyk.eu>
2022-01-03 18:13:36 +0100

    Add container General page

--- src/routes/containers/[id]/__layout.svelte
+++ src/routes/containers/[id]/__layout.svelte
@@ -3,7 +3,7 @@
 	import {containers} from '$lib/stores/containers';
 
 	import CubeIcon from '$icons/cube.svg';
-	import ChartBarIcon from '$icons/chart-bar.svg';
+	import ChartSquareBarIcon from '$icons/chart-square-bar.svg';
 	import FolderIcon from '$icons/folder.svg';
 	import DownloadIcon from '$icons/download.svg';
 
@@ -21,10 +21,10 @@
 		<a class="text-3xl opacity-40 hover:text-primary-focus hover:opacity-100" href="/containers">Containers / </a>
 		{$page.params.id}
 		<div class="badge badge-lg float-right mt-1 text-lg h-8 border-none"
-			 class:bg-info={container.state === 'created'}
-			 class:bg-success={container.state === 'running'}
-			 class:bg-warning={container.state === 'paused' || container.state === 'restarting'}
-			 class:bg-error={container.state === 'dead' || container.state === 'removing'}>
+			 class:bg-info={container?.state === 'created'}
+			 class:bg-success={container?.state === 'running'}
+			 class:bg-warning={container?.state === 'paused' || container?.state === 'restarting'}
+			 class:bg-error={container?.state === 'dead' || container?.state === 'removing'}>
 			{container?.state}
 		</div>
 	</div>
@@ -39,7 +39,7 @@
 		</li>
 		<li class:bordered={$page.path === `/containers/${$page.params.id}/stats`}>
 			<a href="/containers/{$page.params.id}/stats">
-				<ChartBarIcon class="w-6 h-6 stroke-2 inline-block mr-2 opacity-80"/>
+				<ChartSquareBarIcon class="w-6 h-6 stroke-2 inline-block mr-2 opacity-80"/>
 				Statistics
 			</a>
 		</li>
@@ -58,7 +58,7 @@
 	</ul>
 </div>
 
-<div class="mx-4 mt-4">
+<div class="mx-4 mt-8">
 	<slot></slot>
 </div>
 
--- src/routes/containers/[id]/index.svelte
+++ src/routes/containers/[id]/index.svelte
@@ -1,1 +1,104 @@
-Index
+<script lang="ts">
+	import {page} from '$app/stores';
+	import {containers, removeContainer, restartContainer, startContainer, stopContainer} from '$lib/stores/containers';
+	import {goto} from '$app/navigation';
+
+	import RefreshIcon from '$icons/refresh.svg';
+	import PauseIcon from '$icons/pause.svg';
+	import PlayIcon from '$icons/play.svg';
+	import TrashIcon from '$icons/trash.svg';
+
+
+	$: container = $containers.find(c => c.names.includes('/' + $page.params.id));
+
+	let loading = false;
+
+	let removeModal = false;
+
+	const start = async () => {
+		loading = true;
+		await startContainer(container.id);
+		loading = false;
+	};
+	const stop = async () => {
+		loading = true;
+		await stopContainer(container.id);
+		loading = false;
+	};
+	const restart = async () => {
+		loading = true;
+		await restartContainer(container.id);
+		loading = false;
+	};
+
+	const openRemoveModal = () => {
+		removeModal = true;
+	};
+
+	const closeRemoveModal = () => {
+		removeModal = false;
+	};
+
+	const remove = async () => {
+		removeModal = false;
+		loading = true;
+		await removeContainer(container.id);
+		loading = false;
+		await goto('/containers');
+	};
+
+</script>
+
+<div class="card shadow-md bg-base-100 mb-6">
+	<div class="card-body p-6 pt-5">
+		<h2 class="card-title text-xl">State: {container?.state.toUpperCase()}</h2>
+		<p>Status: {container?.status}</p>
+		<div class="card-actions">
+			<button class="btn btn-primary justify-start px-3 text-lg mr-3"
+					on:click={start} class:loading={loading} disabled={loading || container?.state === 'running'}>
+				{#if !loading}<PlayIcon class="h-6 w-6 mr-3 stroke-2"/>{/if}
+				<span class="mt-0 mr-2">Start</span>
+			</button>
+			<button class="btn btn-primary justify-start px-3 text-lg mr-3"
+					on:click={stop} class:loading={loading} disabled={loading || container?.state !== 'running'}>
+				{#if !loading}<PauseIcon class="h-6 w-6 mr-3 stroke-2"/>{/if}
+				<span class="mt-0 mr-2">Stop</span>
+			</button>
+			<button class="btn btn-primary justify-start px-3 text-lg mr-3"
+					on:click={restart} class:loading={loading} disabled={loading || container?.state !== 'running'}>
+				{#if !loading}<RefreshIcon class="h-6 w-6 mr-3 stroke-2"/>{/if}
+				<span class="mt-0 mr-2">Restart</span>
+			</button>
+			<button class="btn btn-primary justify-start px-3 text-lg mr-3"
+					on:click={openRemoveModal} class:loading={loading} disabled={loading || container?.state === 'running'}>
+				{#if !loading}<TrashIcon class="h-6 w-6 mr-3 stroke-2"/>{/if}
+				<span class="mt-0 mr-2">Remove</span>
+			</button>
+			<input type="checkbox" id="my-modal-2" class="modal-toggle" bind:checked={removeModal}>
+			<div class="modal">
+				<div class="modal-box">
+					<p>Do you really want to remove container
+						<span class="font-bold">{container?.names[0].substring(1)}</span> ?
+					</p>
+					<div class="modal-action">
+						<button class="btn btn-primary" on:click={remove}>Remove</button>
+						<button class="btn" on:click={closeRemoveModal}>Cancel</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="card shadow-md bg-base-100 mb-6">
+	<div class="card-body p-6 pt-5">
+		<h2 class="card-title text-xl">Image</h2>
+		<p>Name: {container?.imageName}</p>
+		<p>ID: {container?.imageId}</p>
+	</div>
+</div>
+
+<style lang="scss">
+
+</style>
