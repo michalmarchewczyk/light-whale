0268ea5cc2eb72d3fb37470cda85212abb6ccc90
Michał Marchewczyk <dev@marchewczyk.eu>
2022-01-11 22:59:36 +0100

    Add containers list to image page

--- src/routes/images/[id]/containers.svelte
+++ src/routes/images/[id]/containers.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
+	import type {Container} from '$lib/stores/containers';
+	import {Image, images} from '$lib/stores/images';
+	import {page} from '$app/stores';
+	import ContainerItem from '../../containers/_ContainerItem.svelte';
+	import {containers} from '$lib/stores/containers';
+
+	let image:Image;
+
+	$: image = $images.find(i => i.id.startsWith('sha256:'+$page.params.id));
+
+	let usedContainers:Container[];
+
+	$: usedContainers = $containers?.filter(c => c.imageId === image.id) ?? [];
 
 </script>
 
-Containers
+{#each usedContainers as container}
+	<ContainerItem container={container}/>
+{:else}
+	<p class="w-full text-center text-2xl pt-8 opacity-80">Image isn't used in any containers</p>
+{/each}
 
 <style lang="scss">
 
