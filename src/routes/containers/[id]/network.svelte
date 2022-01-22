d4713f6cc89ad382be7c1ffa0bc4c4354d5e6593
Michał Marchewczyk <dev@marchewczyk.eu>
2022-01-22 18:56:22 +0100

    Add list of sites to container page

--- src/routes/containers/[id]/network.svelte
+++ src/routes/containers/[id]/network.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
 	import {page} from '$app/stores';
 	import {Container, containers,} from '$lib/stores/containers';
+	import type {Site} from '$lib/stores/sites';
+	import {sites} from '$lib/stores/sites';
+	import SiteItem from '$lib/components/network/SiteItem.svelte';
 
 	let container:Container;
 
 	$: container = $containers.find(c => c.names.includes('/' + $page.params.id));
 
 	$: network = container?.networks['light-whale-network'] ?? null;
+
+	let connectedSites:Site[];
+
+	$: connectedSites = $sites.filter(s => container?.id.startsWith(s.containerId));
 
 </script>
 
@@ -24,6 +31,17 @@
 			</p>
 		{/if}
 	</div>
+</div>
+
+<div>
+	<div class="text-2xl font-bold pb-0 mx-6 pt-2 opacity-80">
+		Sites
+	</div>
+	{#each connectedSites as site}
+		<SiteItem site={site}/>
+	{:else}
+		<p class="w-full text-center text-2xl pt-8 opacity-80">No sites are connected to this container</p>
+	{/each}
 </div>
 
 
