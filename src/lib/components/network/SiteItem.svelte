a8759853f7986fe54ad71f2a5c2af669db245163
Michał Marchewczyk <dev@marchewczyk.eu>
2022-01-24 00:01:27 +0100

    Add filtering and sorting to sites list

--- src/lib/components/network/SiteItem.svelte
+++ src/lib/components/network/SiteItem.svelte
@@ -59,6 +59,7 @@
 	<div class="mx-1 sm:mx-2 w-16 sm:w-20 flex-shrink-0"
 		 class:text-error={!online}
 		 class:text-success={online}
+		 class:text-warning={site?.paused}
 	>
 		{#if online}
 			<StatusOnlineIcon class="w-8 h-8 sm:h-10 sm:w-10 mx-auto mt-0 stroke-[1.5px]"/>
--- src/routes/sites/index.svelte
+++ src/routes/sites/index.svelte
@@ -1,12 +1,41 @@
 <script lang="ts">
-	import {sites} from '$lib/stores/sites';
+	import {Site, sites} from '$lib/stores/sites';
 	import SiteItem from '$lib/components/network/SiteItem.svelte';
 	import {containers} from '$lib/stores/containers';
+	import {paramsToLink} from '$lib/utils/paramsToLink';
+	import {page} from '$app/stores';
 
 
 	let onlineCount:number;
 	$: onlineCount = $sites.filter(s =>
 		!s.paused && $containers?.find(c => c.id.startsWith(s.containerId))?.state === 'running').length;
+
+	let filteredSites:Site[];
+
+	$: {
+		filteredSites = $sites;
+		if($page.url.searchParams.get('status') === 'online'){
+			filteredSites = filteredSites.filter(s => !s.paused && $containers?.find(c => c.id.startsWith(s.containerId))?.state === 'running');
+		}
+		if($page.url.searchParams.get('status') === 'disabled'){
+			filteredSites = filteredSites.filter(s => s.paused);
+		}
+		if($page.url.searchParams.get('status') === 'offline'){
+			filteredSites = filteredSites.filter(s => !s.paused && $containers?.find(c => c.id.startsWith(s.containerId))?.state !== 'running');
+		}
+		if($page.url.searchParams.get('sort')){
+			let sortName = $page.url.searchParams.get('sort');
+			let sortOrder = $page.url.searchParams.get('order');
+			if(sortName === 'name'){
+				filteredSites = filteredSites.sort((a,b) => a.domain < b.domain ? 1 : -1);
+			}else if(sortName === 'created'){
+				filteredSites = filteredSites.sort((a,b) => a.created < b.created ? 1 : -1);
+			}
+			if(sortOrder === 'asc'){
+				filteredSites = filteredSites.reverse();
+			}
+		}
+	}
 
 </script>
 
@@ -20,10 +49,63 @@
 		<div class="badge badge-lg float-right mt-1 text-lg h-8">
 			{onlineCount} / {$sites.length} online
 		</div>
+		<div class="w-full mt-6 text-base font-normal">
+			<div class="float-right">
+				<span class="text-lg font-semibold align-middle">Sort:</span>
+				<div class="dropdown">
+					<button class="select select-bordered bg-base-100 align-middle ml-2 w-44 capitalize">
+						{#if $page.url.searchParams.get('sort')}
+							<span class="mt-2 text-base">{$page.url.searchParams.get('sort')} ({$page.url.searchParams.get('order')})</span>
+						{:else}
+							<span class="mt-2 text-base">Created (desc)</span>
+						{/if}
+					</button>
+					<ul class="menu dropdown-content bg-base-100 rounded-box shadow-xl font-semibold w-44 ml-2">
+						<li><a href="{paramsToLink($page.url.search, {sort: 'created', order: 'asc'})}">
+							Created (asc)
+						</a></li>
+						<li><a href="{paramsToLink($page.url.search, {sort: 'created', order: 'desc'})}">
+							Created (desc)
+						</a></li>
+						<li><a href="{paramsToLink($page.url.search, {sort: 'name', order: 'asc'})}">
+							Name (asc)
+						</a></li>
+						<li><a href="{paramsToLink($page.url.search, {sort: 'name', order: 'desc'})}">
+							Name (desc)
+						</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<span class="text-lg font-semibold align-middle">Status:</span>
+			<div class="dropdown">
+				<button class="select select-bordered bg-base-100 align-middle ml-2 w-36 capitalize">
+					{#if $page.url.searchParams.get('status')}
+						<span class="mt-2 text-base">{$page.url.searchParams.get('status')}</span>
+					{:else}
+						<span class="mt-2 text-base">All</span>
+					{/if}
+				</button>
+				<ul class="menu dropdown-content bg-base-100 rounded-box shadow-xl font-semibold w-36 ml-2">
+					<li><a href="{paramsToLink($page.url.search, {status: 'all'})}">
+						All
+					</a></li>
+					<li><a href="{paramsToLink($page.url.search, {status: 'online'})}">
+						Online
+					</a></li>
+					<li><a href="{paramsToLink($page.url.search, {status: 'disabled'})}">
+						Disabled
+					</a></li>
+					<li><a href="{paramsToLink($page.url.search, {status: 'offline'})}">
+						Offline
+					</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </div>
 <div class="p-8 pt-2">
-	{#each $sites as site}
+	{#each filteredSites as site}
 		<SiteItem site={site}/>
 	{:else}
 		<p class="w-full text-center text-3xl pt-12 opacity-50">No sites</p>
