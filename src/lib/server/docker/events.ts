7af880dc73a8b8c3855228accdbc1856274aa7b0
Michał Marchewczyk <dev@marchewczyk.eu>
2022-04-17 01:11:14 +0200

    Filter out unnecessary Docker events

--- src/lib/server/docker/events.ts
+++ src/lib/server/docker/events.ts
@@ -1,9 +1,11 @@
 import {DOCKER_URL} from '$lib/server/docker/config';
+import {logger, LogType} from '$lib/server/utils/Logger';
 
 let lastEvent = null;
 
 export const getEvents = async ():Promise<Record<string, unknown>[]> => {
 	try {
+		logger.log(LogType.Verbose, 'Retrieving Docker events');
 		const serverTimeRes = await fetch(DOCKER_URL + '/info');
 		const serverTime = new Date((await serverTimeRes.json())['SystemTime']).getTime() ?? Date.now();
 		const timestamp = Math.floor(serverTime / 1000);
@@ -11,12 +13,13 @@ export const getEvents = async ():Promise<Record<string, unknown>[]> => {
 			{mode: 'no-cors'},
 		);
 		const data = (await res.text()).split('\n');
-		const events = [];
+		let events = [];
 		for (const v of data) {
 			if (v) {
 				events.push(JSON.parse(v));
 			}
 		}
+		events = events.filter(e => e.Action !== 'top');
 		if (events.length > 0) {
 			lastEvent = events[events.length - 1];
 		}
